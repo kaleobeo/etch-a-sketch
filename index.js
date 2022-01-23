bdf0c699e65f529b8815aeefbb2a4154939073f2
Kaleo K <kaleo.kronberg@gmail.com>
2022-01-22 17:54:31 -0800

    Add labeled buttons and random RGB mode
    Add functionality to use random colors instead of red, not sure if this
    method of getting random rgb is optimal

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ let sketchBox = document.querySelector('.sketchbox')
 let sideLength = 16
 let pixel = document.createElement('div')
 pixel.classList.add("pixel")
+let mode = "normal"
 newGrid(16)
 
 
-
+function randomRGB() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + "," + g + "," + b + ")"
+}
 
 function newGrid(size) {
 
@@ -24,6 +30,10 @@ function newGrid(size) {
     }
 }
 
-sketchBox.addEventListener('mouseover', (e) => e.target.classList.add('hover'))
+sketchBox.addEventListener('mouseover', (e) => {
+    if (mode === "normal") {
+        e.target.classList.add('hover')
+    } else if (mode === "random") e.target.style.backgroundColor = `${randomRGB()}`
+});
 
 
